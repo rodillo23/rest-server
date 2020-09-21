00f0a77d1957140c380ee724f78eb19527517559
Rodolfo Figueroa <ficr23@hotmail.com>
2020-09-21 11:42:43 -0500

    Rest server Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,18 @@
+//Puerto
+
 process.env.PORT = process.env.PORT || 3000
+
+//Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev' 
+
+//Base de Datos
+
+let urlDB
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+}else{
+    urlDB = 'mongodb+srv://samsara:RFOXAHiCyPF4eDLt@cluster0.xq0h4.mongodb.net/cafe?retryWrites=true&w=majority'
+}
+console.log(process.env.NODE_ENV)
+process.env.URLDB = urlDB
--- server/server.js
+++ server/server.js
@@ -1,40 +1,25 @@
 require('./config/config')
 
 const express = require('express')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
 const app = express()
+
+const bodyParser = require('body-parser')
+
 //middlewares-> funciones que se disparan cada que pasa por aqui el codigo
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(require('./routes/usuario'))
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario')
+mongoose.connect(
+    process.env.URLDB, 
+    {useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex: true, useFindAndModify: false}, 
+    (err, res) => {
+        if (err) throw new err;
+        console.log('Base de Datos ONLINE');
 })
 
-app.post('/usuario', (req, res) => {
-    let body = req.body
-    if(body.nombre == undefined){
-        res.status(400).json({
-            ok : false,
-            mensaje: 'El nombre es necesario'
-        })
-    }else{
-        res.json({
-            persona : body
-        })
-    }
-})
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete usuario')
-})
 app.listen(process.env.PORT, ()=> {
     console.log('Escuchando en el puerto', process.env.PORT)
 })

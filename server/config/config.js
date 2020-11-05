b71525d2ad6c207b99f64f98ef3097709c2252b0
Rodolfo Figueroa <ficr23@hotmail.com>
2020-11-05 14:27:43 -0600

    ajuste de variables de entorno para produccion

--- server/config/config.js
+++ server/config/config.js
@@ -17,11 +17,11 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'
 //Base de Datos
 
 let urlDB
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
-}else{
+//if (process.env.NODE_ENV === 'dev') {
+//    urlDB = 'mongodb://localhost:27017/cafe'
+//}else{
     urlDB = process.env.MONGO_URI
-}
+//}
 console.log(process.env.NODE_ENV)
 process.env.URLDB = urlDB
 
